0a9e08526ada9b3d35615e5f1033b3a1245c1069
acc15 <vyacheslav.mayorov@gmail.com>
2018-04-24 01:18:59 +0300

    markdown demo

--- src/main/webapp/chat/src/chat/Msg.jsx
+++ src/main/webapp/chat/src/chat/Msg.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import moment from "moment";
 import classnames from 'classnames';
+import ReactMarkdown from 'react-markdown';
 
 class Msg extends React.Component {
 
@@ -30,7 +31,8 @@ class Msg extends React.Component {
     render() {
         const { msg, user } = this.props;
         return <div className={classnames("msg", "msg-" + msg.type)} style={{opacity: this.state.live}}>
-            <span className="time">{moment(msg.time).format('DD.MM.YYYY HH:mm:ss.SSS')}</span> { msg.user && <span className={classnames("user", user.id === msg.user.id && "me")}>{msg.user.name}</span> } <span className="text">{msg.text}</span>
+            <span className="time">{moment(msg.time).format('DD.MM.YYYY HH:mm:ss.SSS')}</span> { msg.user && <span className={classnames("user", user.id === msg.user.id && "me")}>{msg.user.name}</span> } <ReactMarkdown source={msg.text}/>
+
         </div>;
     }
 
--- src/main/webapp/chat/src/chat/MsgInput.jsx
+++ src/main/webapp/chat/src/chat/MsgInput.jsx
@@ -23,7 +23,7 @@ class MsgInput extends React.Component {
     render() {
         return <form onSubmit={this.onPost}>
             <div className="input-group mb-3">
-                <input type="text" className="form-control" onChange={this.onMsgChange} value={this.state.msg} placeholder="Type your message here"/>
+                <textarea className="form-control" onChange={this.onMsgChange} value={this.state.msg} placeholder="Type your message here"/>
                 <div className="input-group-append">
                     <button className="btn btn-primary" type="submit">Send</button>
                 </div>
